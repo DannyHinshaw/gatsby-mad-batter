c5d39c019c9de648a4f7d8b4857063662d78eb02
Danny Hinshaw <daniel.hinshaw@array.com>
2021-05-26 19:12:11 -0400

    Add new logos, change nav menu color, swap out main panel logo.

--- src/components/MainPanel/index.tsx
+++ src/components/MainPanel/index.tsx
@@ -2,14 +2,16 @@ import BackgroundImage, { IFluidObject } from "gatsby-background-image";
 import * as React from "react";
 import { ComponentType } from "react";
 import { ISectionProps } from "../../pages";
-import "./MainPanel.scss";
 
+// @ts-ignore
+import Logo from "./mad-batter-header-logo.png";
+import "./MainPanel.scss";
 
 const MainPanel: ComponentType<ISectionProps> = ({ bgImage }): JSX.Element => (
 	<BackgroundImage fluid={bgImage} Tag="section" id="main">
 		<div id="logoContainer">
 			<img
-				src="/logos/logo_transparent_background_white(hat_below).png"
+				src={Logo}
 				style={{ maxWidth: "1600px" }}
 				id="mainLogo"
 				alt="Logo" />
